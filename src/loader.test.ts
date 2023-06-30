c54bbfe158e61f8992c0587cddf70980f7fbe176
Jeffrey Chupp <jeff@semanticart.com>
2023-06-30 09:21:28 -0400

    Support polling via `prefab.poll({frequencyInMs})`
    Also use fetch-mock (and drop express dev dependency)

--- src/loader.test.ts
+++ src/loader.test.ts
@@ -1,10 +1,10 @@
-/* eslint-disable-next-line import/no-extraneous-dependencies */
-import express from "express";
 import Context from "./context";
 import Loader from "./loader";
 import version from "./version";
+import { wait } from "../test/wait";
 
-import FetchMock from "../test/fetchMock";
+import fetchMock, { enableFetchMocks } from "jest-fetch-mock";
+enableFetchMocks();
 
 const context = new Context({
   user: { id: "123", email: "test@example.com" },
@@ -39,48 +39,37 @@ describe("overriding endpoints", () => {
 });
 
 describe("load", () => {
-  beforeEach(() => {
-    jest.useFakeTimers();
-  });
-
-  afterEach(() => {
-    jest.runAllTimers();
-  });
-
   describe("when the timeout is reached", () => {
-    let server: any;
-
-    const TIMEOUT = 500;
-    const PORT = 8675;
-
-    beforeEach(() => {
-      const app = express();
-
-      app.get("/too-long/configs/eval-with-context/*", (_, res: any) => {
-        setTimeout(() => {
-          res.send('{ "values": { "failover": { "bool": false } } }');
-        }, TIMEOUT + 1000);
-      });
-
-      app.get("/failover/configs/eval-with-context/*", (_, res: any) => {
-        res.send('{ "values": { "failover": { "bool": true } } }');
-      });
-
-      app.get("/heartbeat", (_, res: any) => {
-        res.send("OK");
-      });
-
-      server = app.listen(PORT, () => {});
-    });
-
-    afterEach((done) => {
-      server.close(done);
-    });
+    const TIMEOUT = 25;
 
     it("can successfully return from a second endpoint if the first times out", async () => {
+      fetchMock.mockResponse(async (req) => {
+        const url = new URL(req.url);
+
+        if (url.pathname.startsWith("/too-long/")) {
+          return new Promise((resolve) => {
+            setTimeout(() => {
+              resolve({
+                status: 200,
+                body: JSON.stringify({ values: { failover: { bool: false } } }),
+              });
+            }, TIMEOUT * 2);
+          });
+        }
+
+        if (url.pathname.startsWith("/failover/")) {
+          return {
+            status: 200,
+            body: JSON.stringify({ values: { failover: { bool: true } } }),
+          };
+        }
+
+        throw `Unexpected URL: ${url.toString()}`;
+      });
+
       const endpoints = [
-        `http://localhost:${PORT}/too-long`,
-        `http://localhost:${PORT}/failover`,
+        `http://localhost/too-long`,
+        `http://localhost/failover`,
       ];
 
       loader = new Loader({
@@ -92,10 +81,9 @@ describe("load", () => {
 
       const promise = loader.load();
 
-      // let time pass
-      jest.runAllTimers();
+      await wait(TIMEOUT);
 
-      const results = (await promise) as { [key: string]: any };
+      const results = (await promise) as { failover: { bool: boolean } };
       expect(results.failover.bool).toStrictEqual(true);
     });
   });
@@ -114,65 +102,104 @@ describe("load", () => {
     };
 
     it("can succesfully return results from the first endpoint", async () => {
-      const fetchMock = new FetchMock(() =>
-        Promise.resolve({
-          ok: true,
-          json: () => Promise.resolve(data),
-        })
-      );
+      let fetchCount = 0;
+      let requestUrl: URL | undefined;
+      let requestHeaders = new Map();
+
+      fetchMock.mockResponse(async (req) => {
+        fetchCount++;
+
+        requestUrl = new URL(req.url);
+
+        requestHeaders.set("Authorization", req.headers.get("Authorization"));
+        requestHeaders.set(
+          "X-PrefabCloud-Client-Version",
+          req.headers.get("X-PrefabCloud-Client-Version")
+        );
+
+        return {
+          status: 200,
+          body: JSON.stringify(data),
+        };
+      });
 
       loader = new Loader({ context, apiKey });
 
       const results = await loader.load();
-
-      expect(fetchMock.requestCount).toStrictEqual(1);
       expect(results).toStrictEqual(data.values);
-      expect(fetchMock.lastUrl?.host).toStrictEqual(
-        "api-prefab-cloud.global.ssl.fastly.net"
-      );
-      expect(fetchMock.lastUrl?.pathname).toStrictEqual(
+      expect(fetchCount).toStrictEqual(1);
+
+      if (!requestUrl || !requestHeaders) {
+        throw "Fetch hasn't happened";
+      }
+
+      expect(requestUrl.host).toEqual("api-prefab-cloud.global.ssl.fastly.net");
+
+      expect(requestUrl.pathname).toStrictEqual(
         "/api/v1/configs/eval-with-context/eyJjb250ZXh0cyI6W3sidHlwZSI6InVzZXIiLCJ2YWx1ZXMiOnsiaWQiOnsic3RyaW5nIjoiMTIzIn0sImVtYWlsIjp7InN0cmluZyI6InRlc3RAZXhhbXBsZS5jb20ifX19LHsidHlwZSI6ImRldmljZSIsInZhbHVlcyI6eyJtb2JpbGUiOnsiYm9vbCI6dHJ1ZX19fV19"
       );
-      expect(fetchMock.lastRequestOptions?.headers).toStrictEqual({
-        Authorization: "Basic dTphcGlLZXk=",
-        "X-PrefabCloud-Client-Version": `prefab-cloud-js${version}`,
-      });
+
+      expect(requestHeaders.get("Authorization")).toStrictEqual(
+        "Basic dTphcGlLZXk="
+      );
+      expect(requestHeaders.get("X-PrefabCloud-Client-Version")).toStrictEqual(
+        `prefab-cloud-js${version}`
+      );
     });
 
     it("can successfully return from a second endpoint if the first fails", async () => {
-      const fetchMock = new FetchMock(
-        ({ requestCount }: { requestCount: number }) => {
-          if (requestCount < 2) {
-            return Promise.reject(new Error("Network error"));
-          }
+      let fetchCount = 0;
+      let requestUrl: URL | undefined;
 
-          return Promise.resolve({
-            ok: true,
-            json: () => Promise.resolve(data),
-          });
+      fetchMock.mockResponse(async (req) => {
+        fetchCount++;
+
+        if (fetchCount < 2) {
+          return Promise.reject(new Error("Network error"));
         }
-      );
+
+        requestUrl = new URL(req.url);
+
+        return {
+          status: 200,
+          body: JSON.stringify(data),
+        };
+      });
 
       loader = new Loader({ context, apiKey });
 
       const results = await loader.load();
 
-      expect(fetchMock.requestCount).toStrictEqual(2);
+      expect(fetchCount).toStrictEqual(2);
       expect(results).toStrictEqual(data.values);
-      expect(fetchMock.lastUrl?.host).toStrictEqual("api.prefab.cloud");
+
+      if (!requestUrl) {
+        throw "Last fetch hasn't happened";
+      }
+      expect(requestUrl.host).toStrictEqual("api.prefab.cloud");
     });
 
     it("fails when no endpoints are reachable", async () => {
-      const fetchMock = new FetchMock(() =>
-        Promise.reject(new Error("Network error"))
-      );
+      let fetchCount = 0;
+      let requestUrl: URL | undefined;
+
+      fetchMock.mockResponse(async (req) => {
+        fetchCount++;
+        requestUrl = new URL(req.url);
+        return Promise.reject(new Error("Network error"));
+      });
 
       loader = new Loader({ context, apiKey });
 
       loader.load().catch((reason: any) => {
         expect(reason.message).toEqual("Network error");
-        expect(fetchMock.requestCount).toStrictEqual(2);
-        expect(fetchMock.lastUrl?.host).toStrictEqual("api.prefab.cloud");
+        expect(fetchCount).toStrictEqual(2);
+
+        if (!requestUrl) {
+          throw "Last fetch hasn't happened";
+        }
+
+        expect(requestUrl.host).toStrictEqual("api.prefab.cloud");
       });
     });
   });

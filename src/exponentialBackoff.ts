f3344623b18bb9d2e7cc0be9d1fd8a5ae83ad813
ayip8 <16192761+ayip8@users.noreply.github.com>
2023-10-31 09:14:03 -0400

    Fix time units

--- src/exponentialBackoff.ts
+++ src/exponentialBackoff.ts
@@ -7,6 +7,7 @@ export class ExponentialBackoff {
 
   private delay: number;
 
+  // arguments are in seconds
   constructor(maxDelay: number, initialDelay = 2, multiplier = 2) {
     this.initialDelay = initialDelay;
     this.maxDelay = maxDelay;
@@ -17,6 +18,6 @@ export class ExponentialBackoff {
   call(): number {
     const delayValue = this.delay;
     this.delay = Math.min(this.delay * this.multiplier, this.maxDelay);
-    return delayValue;
+    return delayValue * 1000;
   }
 }

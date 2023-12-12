cc4713de0da29ee82de823ed8a8df9e3a49afece
Jeffrey Chupp <jeff@semanticart.com>
2023-12-12 10:15:42 -0500

    chore: Remove identity
    This has been deprecated since 2023-05-01

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ import { prefab } from "./src/prefab";
 import { Config } from "./src/config";
 import ConfigValue from "./src/configValue";
 import Context from "./src/context";
-import Identity from "./src/identity";
 import version from "./src/version";
 
-export { prefab, Config, ConfigValue, Context, Identity, version };
+export { prefab, Config, ConfigValue, Context, version };

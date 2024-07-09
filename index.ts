217e52c285a9a7276f66b04a4fbd15dad91eb238
ayip8 <16192761+ayip8@users.noreply.github.com>
2024-07-09 16:51:34 -0400

    Adds support for collectContextMode options

--- index.ts
+++ index.ts
@@ -3,6 +3,17 @@ import { Config } from "./src/config";
 import ConfigValue, { Duration } from "./src/configValue";
 import Context from "./src/context";
 import ContextValue from "./src/contextValue";
+import { CollectContextModeType } from "./src/loader";
 import version from "./src/version";
 
-export { prefab, Config, ConfigValue, Context, ContextValue, Duration, Prefab, version };
+export {
+  prefab,
+  CollectContextModeType,
+  Config,
+  ConfigValue,
+  Context,
+  ContextValue,
+  Duration,
+  Prefab,
+  version,
+};

9c0d8bb7135ae8aa70724cbc6e69ebae28285d07
Jeffrey Chupp <jeff@semanticart.com>
2024-08-20 13:24:25 -0400

    Handle non-Latin1 characters in Base64 encoding
    Hat tip to https://developer.mozilla.org/en-US/docs/Glossary/Base64#the_unicode_problem

--- src/base64Encode.ts
+++ src/base64Encode.ts
@@ -1,6 +1,12 @@
 const base64Encode = (obj: string) => {
   if (typeof window !== "undefined") {
-    return window.btoa(obj);
+    if (typeof TextEncoder === "undefined") {
+      return window.btoa(obj);
+    }
+
+    const bytes = new TextEncoder().encode(obj);
+    const binString = Array.from(bytes, (byte) => String.fromCodePoint(byte)).join("");
+    return btoa(binString);
   }
 
   return Buffer.from(obj).toString("base64");
